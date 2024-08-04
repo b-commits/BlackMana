7e21d12bed96f1acf2032203412bb0fb5f353350
b-commits <s19677@pjwstk.edu.pl>
2024-08-04 18:33:23 +0200

    BM-7 Simplified SelectableManager logic

--- Common/Interfaces/Interfaces.cs
+++ Common/Interfaces/Interfaces.cs
@@ -9,6 +9,18 @@ internal interface ISelectable
     public Vector2I MapPosition { get; set; }
     public void OnSelect();
     public void OnDeselect();
+    
+    public void Select()
+    {
+        Selected = true;
+        OnSelect();
+    }
+    
+    public void Deselect()
+    {
+        Selected = false;
+        OnDeselect();
+    }
 }
 
 internal interface IMovable
